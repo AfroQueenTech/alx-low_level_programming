34b241934646cf470699d23abfbca6f4573de27c
Pakita Malila <pakita.malila@gmail.com>
2023-03-21 14:33:58 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
-puts("\"Programming is like building a multilingual puzzle\n");
+#include <stdio.h>
+/*
+ * main - Program that prints a sentence using the function puts.
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	
+	return (0);
+}

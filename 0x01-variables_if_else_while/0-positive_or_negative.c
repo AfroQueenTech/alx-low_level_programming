ba68280dfa16c348bf87dd1e2dfb9cdc3c913bbc
Pakita Malila <pakita.malila@gmail.com>
2023-03-25 22:52:09 +0000

    pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 
+ * Return: Always 0
  * Program that assigns a random number to a variable n.
- * betty style doc for function main goes there 
+ * betty style doc for function main goes there
  */
 int main(void)
 {

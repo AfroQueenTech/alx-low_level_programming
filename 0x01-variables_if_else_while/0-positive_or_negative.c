cbd79575dee9c3f8ab6609598b2531cc94ebe39e
Pakita Malila <pakita.malila@gmail.com>
2023-03-25 22:34:20 +0000

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n > 0){
+	if (n > 0)
 		printf("%d\n is positive");
-	}{
-	else if (n == 0) {
+	else if (n == 0)
 		printf("%d\n is zero");
-	}{
 	else
-		printf("%d\n is negative")
-	}
-	return (0)
+		printf("%d\n is negative");
+	return (0);

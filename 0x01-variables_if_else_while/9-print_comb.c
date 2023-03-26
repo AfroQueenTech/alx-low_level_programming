2d6a01f1c6384444a15c25c815c0c94612a89f1a
Pakita Malila <pakita.malila@gmail.com>
2023-03-26 17:32:43 +0000

    print combos

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar(num + 48);
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);

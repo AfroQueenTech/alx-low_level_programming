ed0c3efcca506face23d5716bb3bfcebb698b972
Pakita Malila <pakita.malila@gmail.com>
2023-03-26 17:13:15 +0000

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ * Program that prints all possible combos of single-digit numbers.
+ */
+int main(void)
+{
+	int nums;
+
+	for (nums = 0; nums <= 9, num++)
+	{
+		putchar((num % num), ", ");
+	}
+	putchar('\n');
+	return (0);
+}
